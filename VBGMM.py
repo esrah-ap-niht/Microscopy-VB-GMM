739385981ebc098d5c4d3b8e550131073807e091
esrah-ap-niht <86078445+esrah-ap-niht@users.noreply.github.com>
2022-03-02 15:47:28 -0500

    Update VBGMM.py
    Simplified correlation matrix to be more easily interpreted, removed covariance portion of matrix, moved average composition to Y axis to have cleaner visuals.
    
    Added Class Table output to aid in determining what classes to inspect first.
    
    Fixed error with appending new data points to training subset. Previously points were selected with np.sample which prevents appending more data points than are in the uncertainty subset. By changing to np.choose uncertainty candidate points can be selected multiple times, which decreases the number of training iterations necessary and therefore speeds up analysis.

--- VBGMM.py
+++ VBGMM.py
@@ -16,15 +16,6 @@ from sklearn import mixture
 import scipy
 import seaborn as sns
 from scipy.cluster.hierarchy import dendrogram
-import time 
-import ray
-import psutil
-
-num_cpus = psutil.cpu_count(logical=False)
-ray.init(num_cpus=num_cpus)
-
-
-#from Utility_Functions import * 
 
 # Default settings 
 gc.enable()
@@ -33,9 +24,9 @@ root = Tk()
 root.withdraw() 
 
 # Define functions needed later 
-@ray.remote
-def score_samples(indexes): 
-    return DPGMM.score_samples( full_data[indexes,:] )
+
+def score_samples(data): 
+    return DPGMM.score_samples( data )
 
 # Conversion of covariance matrix to correlation matrix (i.e. normalization)
 def correlation_from_covariance(covariance):
@@ -313,6 +304,7 @@ while run:
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.05)
     plt.colorbar(plot, cax=cax)
+    plt.tight_layout()
     plt.savefig("Sum of Intensities" + ".png", dpi=300)
     plt.close(fig)
     gc.collect()
@@ -322,10 +314,7 @@ while run:
     histogram_size = 5_000
     hist_data = pd.DataFrame(data = None, columns = ["Data", "Element"])
     
-    for i, layer in enumerate(image_list): 
-      #  res = sum(map(lambda i: i == True, (np.sum(array, axis = 2) > eds_quality_threshold).flatten()))
-      #  data = np.array( [  (random.sample(list(array[:, :, i][np.sum(array, axis = 2) > eds_quality_threshold].flatten() ), min(histogram_size, res) ) )  ,  [layer] *min(histogram_size, res) ]  ).T
-        
+    for i, layer in enumerate(image_list):      
         res = array.shape[0]*array.shape[1]
         data = np.array( [  (random.sample(list(array[:, :, i].flatten() ), min(histogram_size, res) ) )  ,  [layer] *min(histogram_size, res) ]  ).T
        
@@ -350,6 +339,7 @@ while run:
     plt.xlabel("Element Intensity")
     plt.ylabel("Relative Frequency")
     plt.title('Histograms of Intensities of Filtered Pixels')
+    plt.tight_layout()
     plt.savefig("Histograms of Intensities of Filtered Pixels" + ".png", dpi=300)
     plt.close(fig)
     del hist_data
@@ -382,7 +372,6 @@ while run:
         # Train VB-GMM model on training subset
         DPGMM.fit(training_data)
         
-        
         # Reweight model classes based on entire dataset 
         initial_weights = DPGMM.weights_
         cons = {'type':'eq', 'fun': constraint}
@@ -400,28 +389,26 @@ while run:
         os.mkdir(gmm_name)
         os.chdir(gmm_name)
         np.save(gmm_name + '_weights', DPGMM.weights_, allow_pickle=False)
-        np.save(gmm_name + '_means', DPGMM.means_, allow_pickle=False)
+        np.save(gmm_name + '_means', DPGMM.precisions_cholesky_, allow_pickle=False)
         np.save(gmm_name + '_covariances', DPGMM.covariances_, allow_pickle=False)
         
-        available_memory = psutil.virtual_memory().free/(1073741824) # available memory in GB 
-        scoring_memory_multiplier = 
-        maximum_packet_size = available_memory / (scoring_memory_multiplier * num_cpus)
-        packet_size = min( *scoring_memory_multiplier, maximum_packet_size)
-        
-        
-        sys.getsizeof(full_data)*scoring_memory_multiplier/1073741824
-        
-        
-        ray_data = ray.put(full_data)
-        results = ( ray.get( [ score_samples.remote( ray_data, [images[j] for j in range(len(images) )][k] ) for k in range(len(images))]) )
-
-
-
-
-
-        # Create uncertainty intensity map     
-        uncertainty = DPGMM.score_samples( full_data )
-        
+        try: 
+            uncertainty = DPGMM.score_samples( full_data ) 
+            gc.collect()
+        except MemoryError:
+            mid_point = round(full_data.shape[0] / 3.0)
+            
+            uncertainty = DPGMM.score_samples( full_data[0:mid_point, :] )    
+            uncertainty1 = DPGMM.score_samples( full_data[mid_point: mid_point*2, :] )    
+            uncertainty = np.concatenate( (uncertainty, uncertainty1), axis = 0)
+            del uncertainty1
+            gc.collect()
+            
+            uncertainty1 = DPGMM.score_samples( full_data[mid_point*2: full_data.shape[0], :] )    
+            uncertainty = np.concatenate( (uncertainty, uncertainty1), axis = 0)
+            del uncertainty1
+            gc.collect()
+                
         
         uncertainty = np.asarray(uncertainty).reshape(array.shape[0], array.shape[1])
         np.save('Log_Probabilities.npy', uncertainty, allow_pickle=False)
@@ -444,6 +431,7 @@ while run:
         plt.xticks([])
         plt.yticks([])
         plt.title("Absolute Scaled Log Probabilities", fontsize = fontsize*(0.7))
+        plt.tight_layout()
         plt.savefig("Absolute Scaled Log Probabilities" + ".png")
         plt.close(fig)
         gc.collect()
@@ -466,6 +454,7 @@ while run:
         plt.xticks([])
         plt.yticks([])
         plt.title("Relative Scaled Log Probabilities", fontsize = fontsize*(0.7))
+        plt.tight_layout()
         plt.savefig("Relative Scaled Log Probabilities" + ".png")
         plt.close(fig)
         gc.collect()
@@ -478,6 +467,7 @@ while run:
         plt.xlabel("Element Intensity")
         plt.ylabel("Relative Frequency")
         plt.title('Histograms of Log Probabilities')
+        plt.tight_layout()
         plt.savefig("Histograms of Log Probabilities" + ".png", dpi=300)
         plt.close(fig)
         gc.collect()
@@ -490,20 +480,14 @@ while run:
         ax.set_xlabel('Dirichlet Process Components');
         ax.set_ylabel('Posterior expected mixture weight');
         fig.suptitle("Mixture Weight per Class" ,fontsize=20 )
+        plt.tight_layout()
         plt.savefig("Mixture Weights" + ".png", dpi=300)
         plt.close(fig)
         gc.collect()
     
         # Return up one directory level 
         os.chdir( os.path.dirname(os.getcwd()) )
-        
-        # Due to scipy limtations, we must create a second 'plain' GMM to calculate the AIC or BIC scores 
-        GMM = mixture.GaussianMixture(n_components = number_of_classes)
-        GMM.weights_ = DPGMM.weights_
-        GMM.means_ = DPGMM.means_
-        GMM.covariances_ = DPGMM.covariances_
-        GMM.precisions_cholesky_ = DPGMM.precisions_cholesky_
-    
+
         # Once the model tuning is complete, append new training data if necessary
         if DPGMM.converged_ == False: 
             autodidactic_loop = False
@@ -515,7 +499,7 @@ while run:
             break 
         elif  (np.min(uncertainty) < autodidectic_threshold) and ( (generation + 1) < max_autodidectic_iterations):
             new_data = array[uncertainty <= autodidectic_threshold]
-            new_data = random.sample(list(new_data), int(min(training_set_growth_rate, len(list(new_data)) ) ) ) 
+            new_data = random.sample(list(new_data), int( min(training_set_growth_rate, len(list(new_data))) ) ) 
             new_data = np.asarray(new_data)
             
             print("Current number of training points")
@@ -545,13 +529,52 @@ while run:
     
     # Save updated model
     np.save('Final_Model_Weights', DPGMM.weights_, allow_pickle=False)
-    np.save('Final_Model_Means', DPGMM.means_, allow_pickle=False)
+    np.save('Final_Model_Means', DPGMM.precisions_cholesky_, allow_pickle=False)
     np.save('Final_Model_Covariances', DPGMM.covariances_, allow_pickle=False)
     np.save('Log_Probabilities.npy', uncertainty, allow_pickle=False)
     components = len(DPGMM.weights_)
     
     size = 60
     fontsize = 60
+    
+    # Segment montage data       
+    try: 
+        segmentation = DPGMM.predict(full_data)
+        gc.collect()
+        print("Segmentation: 100%")
+
+    except MemoryError:
+        mid_point = round(full_data.shape[0] / 5.0)
+        
+        segmentation = DPGMM.predict( full_data[0:mid_point, :] )    
+        print("Segmentation: 20%")
+        
+        segmentation1 = DPGMM.predict( full_data[mid_point: mid_point*2, :] )    
+        segmentation = np.concatenate( (segmentation, segmentation1), axis = 0)
+        del segmentation1
+        gc.collect()
+        print("Segmentation: 40%")
+
+        segmentation1 = DPGMM.predict( full_data[mid_point*2: mid_point*3, :] )    
+        segmentation = np.concatenate( (segmentation, segmentation1), axis = 0)
+        del segmentation1
+        gc.collect()
+        print("Segmentation: 60%")
+
+        segmentation1 = DPGMM.predict( full_data[mid_point*3: mid_point*4, :] )    
+        segmentation = np.concatenate( (segmentation, segmentation1), axis = 0)
+        del segmentation1
+        gc.collect()
+        print("Segmentation: 80%")
+
+        segmentation1 = DPGMM.predict( full_data[mid_point*4: full_data.shape[0], :] )    
+        segmentation = np.concatenate( (segmentation, segmentation1), axis = 0)
+        del segmentation1
+        gc.collect()
+        print("Segmentation: 100%")
+
+    segmentation = np.asarray(segmentation).reshape(array.shape[0], array.shape[1])
+    np.save('Segmentation', segmentation, allow_pickle=False)
     
     # apply mask to hide pixels that sum to less than the eds_quality_threshold. 
     # this is used to remove pixels that are shaddowed, artifacts, etc. 
@@ -570,10 +593,11 @@ while run:
         pass 
             
     plt.imshow(mask, cmap = 'brg', alpha = 0.7)
+    plt.tight_layout()
     plt.savefig("EDS Quality Mask", bbox_inches = 'tight', dpi = 300)
     plt.close(fig)
     gc.collect()
-    
+   
     # Create agglomerative hierarchial model and save results 
     fig, ax = plt.subplots(figsize=(size, size))
     heir = sklearn.cluster.AgglomerativeClustering(distance_threshold=0, n_clusters=None, linkage = 'complete')
@@ -582,9 +606,47 @@ while run:
     ax = plt.gca()
     ax.tick_params(axis='x', which='major', labelsize=fontsize)
     ax.tick_params(axis='y', which='major', labelsize=fontsize)
+    plt.tight_layout()
     plt.savefig("Consolidation Dendrogram" + ".png")
     plt.close(fig)
     gc.collect()
+     
+    # Prepare HAM linkage matrix for exporting 
+    # Remove all non-first-level linkages 
+    
+    linkage_matrix = np.delete(linkage_matrix,3,1)
+    
+    # Reformat to three columns 
+    linkage_matrix = np.concatenate( (np.delete(linkage_matrix, 1, 1), np.delete(linkage_matrix, 0, 1) ), axis = 0)
+    
+    index_to_delete = []
+    uniques = np.unique(segmentation)
+    for i, row in enumerate(linkage_matrix[:,0]):
+        if (row in uniques) == False:
+            index_to_delete.append(i)
+            
+    linkage_matrix = np.delete(linkage_matrix, np.array(index_to_delete, dtype = np.int16), 0)
+    
+    linkage_matrix = pd.DataFrame( data = linkage_matrix, columns = ["Class ID", "Dissimilarity Scale"])
+    
+    index_to_delete = []
+    for row in range(len(DPGMM.weights_)):
+        if (row in uniques) == False:
+            index_to_delete.append(row)
+            
+    weights = np.delete(DPGMM.weights_, np.array(index_to_delete, dtype = np.int16), 0)
+
+    linkage_matrix = linkage_matrix.sort_values(by=['Class ID'])
+    linkage_matrix['Area Percent'] = weights * 100.0
+    
+    for j, element in enumerate(image_list): 
+        element_percents = []
+        for i, class_id in enumerate(linkage_matrix['Class ID']): 
+            element_percents.append(DPGMM.means_[int(class_id)][j] )
+        linkage_matrix[str(element)] = element_percents
+        
+    linkage_matrix.to_excel("Class Data.xlsx")
+    
     
     # Create absolute uncertainty intensity map
     fig, ax = plt.subplots(figsize=(size, size), dpi=300)
@@ -604,9 +666,30 @@ while run:
     plt.xticks([])
     plt.yticks([])
     plt.title("Absolute Scaled Log Probabilities", fontsize = fontsize*(0.7))
-    plt.savefig("Absolute Scaled Log Probabilities" + ".png")
+    plt.tight_layout()
+    plt.savefig("Absolute Scaled Log Probabilities with Colorbar" + ".png")
     plt.close(fig)
     gc.collect()
+    
+    
+        # Create absolute uncertainty intensity map
+    fig, ax = plt.subplots(figsize=(size, size), dpi=300)
+    try:
+        plt.imshow(background_image)
+        plot = plt.imshow(uncertainty, cmap = "jet_r", vmin = max(-50, autodidectic_threshold), vmax = 0, alpha = 0.7 )
+    except NameError:
+        plot = plt.imshow(uncertainty, cmap = "jet_r", vmin = max(-50, autodidectic_threshold), vmax = 0 )
+        pass 
+    plt.xticks([])
+    plt.yticks([])
+    plt.title(file)
+    plt.xticks([])
+    plt.yticks([])
+    plt.tight_layout()
+    plt.savefig("Absolute Scaled Log Probabilities without Colorbar" + ".png")
+    plt.close(fig)
+    gc.collect()
+    
     
     # Create relative uncertainty intensity map
     fig, ax = plt.subplots(figsize=(size, size), dpi=300)
@@ -626,7 +709,26 @@ while run:
     plt.xticks([])
     plt.yticks([])
     plt.title("Relative Scaled Log Probabilities", fontsize = fontsize*(0.7))
-    plt.savefig("Relative Scaled Log Probabilities" + ".png")
+    plt.tight_layout()
+    plt.savefig("Relative Scaled Log Probabilities with Colorbar" + ".png")
+    plt.close(fig)
+    gc.collect()
+    
+    
+    # Create relative uncertainty intensity map
+    fig, ax = plt.subplots(figsize=(size, size), dpi=300)
+    try:
+        plt.imshow(background_image)
+        plot = plt.imshow(uncertainty, cmap = "jet_r", alpha = 0.7 )
+    except NameError:
+        plot = plt.imshow(uncertainty, cmap = "jet_r" )
+        pass 
+    plt.xticks([])
+    plt.yticks([])
+    plt.xticks([])
+    plt.yticks([])
+    plt.tight_layout()
+    plt.savefig("Relative Scaled Log Probabilities without Colorbar" + ".png")
     plt.close(fig)
     gc.collect()
     
@@ -638,15 +740,10 @@ while run:
     ax.set_xlabel('Dirichlet Process Components');
     ax.set_ylabel('Posterior expected mixture weight');
     fig.suptitle("Mixture Weight per Class" ,fontsize=20 )
+    plt.tight_layout()
     plt.savefig("Mixture Weights" + ".png", dpi=300)
     plt.close(fig)
     gc.collect()
-    
-    # Segment montage data 
-    segmentation = DPGMM.predict(full_data)
-    segmentation = np.asarray(segmentation).reshape(array.shape[0], array.shape[1])
-    np.save('Segmentation', segmentation, allow_pickle=False)
-    
     
     # Save semantic segmentation map 
     fig, ax = plt.subplots(figsize = (size, size)) 
@@ -666,32 +763,54 @@ while run:
     cax = divider.append_axes("right", size="5%", pad=0.05)
     cbar = plt.colorbar(plot, cax=cax, ticks=np.arange(np.min(segmentation),np.max(segmentation)+1))
     cbar.ax.tick_params(labelsize=25) 
-    plt.title("Class Segmentation")
-    plt.savefig("Class Segmentation" + ".png", dpi=300)
+    plt.tight_layout()
+    plt.title("Class Segmentation", fontsize = fontsize*(0.7))
+    plt.savefig("Class Segmentation with Colorbar" + ".png", dpi=300)
+    plt.close(fig)
+    gc.collect()
+    
+    # Save semantic segmentation map 
+    fig, ax = plt.subplots(figsize = (size, size)) 
+    ax = plt.subplot()
+    cmap = plt.get_cmap('nipy_spectral', np.max(segmentation)-np.min(segmentation)+1)
+    plt.gca().invert_yaxis()
+    try:
+        plt.imshow(background_image)
+        plot = plt.imshow(segmentation, cmap=cmap, alpha = 0.7, vmin = np.min(segmentation)-.5, vmax = np.max(segmentation)+.5)
+    except NameError:
+        plot = plt.imshow(segmentation, cmap=cmap, alpha = 1, vmin = np.min(segmentation)-.5, vmax = np.max(segmentation)+.5)
+        pass 
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.tight_layout()
+    plt.savefig("Class Segmentation without Colorbar" + ".png", dpi=300)
     plt.close(fig)
     gc.collect()
     
     
     # Create binary class maps and save 
     w = (-DPGMM.weights_).argsort()
-    
+
     for ind2 in w:
         
         # Ignore extraneous classes that are not in the segmentation 
-        if ind2 in segmentation: 
+        if ind2 in uniques: 
             
             fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(ratio*size, size), dpi=300)
             stdev = np.sqrt(np.abs(DPGMM.covariances_[ind2]))
             stdev[DPGMM.covariances_[ind2] < 0.0] = -1 * stdev[DPGMM.covariances_[ind2] < 0.0] 
-            annot = np.diag(DPGMM.means_[ind2],0)
+            annot = np.diag(DPGMM.precisions_cholesky_[ind2],0)
             annot = np.round(annot,2)
             annot = annot.astype('str')
             annot[annot=='0.0']=''
-            sns.heatmap(stdev, xticklabels = image_list, yticklabels = image_list, center = 0, linewidths=.5, robust = True, annot = annot, fmt='', linecolor = 'black' ,cmap = 'PRGn', cbar_kws={'label': 'Sqrt(Covariance)', 'orientation': 'horizontal'})
-            sns.heatmap(correlation_from_covariance(DPGMM.covariances_[ind2]), xticklabels = image_list, yticklabels = image_list,center = 0, vmin = -1, vmax = 1, linewidths=.5, linecolor = 'black', cmap = 'seismic', mask = np.tril(stdev), cbar_kws={'label': 'Correlation', 'orientation': 'horizontal'})
+            sns.heatmap(correlation_from_covariance(DPGMM.covariances_[ind2]), xticklabels = image_list, yticklabels = [ str(element) + ": " + str( round(DPGMM.means_[ind2][i], 2) ) for i, element in enumerate(image_list) ], center = 0, vmin = 0, vmax = 1, linewidths=1, linecolor = 'white', cmap = 'bwr', mask = np.triu(stdev), cbar_kws={'label': 'Correlation', 'orientation': 'horizontal'})
             ax2.tick_params(axis='x', pad=5)
             ax2.set_yticklabels( ax2.get_yticklabels(), rotation=0)
-                   
+            
+            for label in (ax2.get_xticklabels() + ax2.get_yticklabels()):
+                label.set_fontsize(60)
+                        
             plt.sca(ax1)
             plot = plt.pcolormesh( np.ma.masked_where(segmentation != ind2, np.ones(segmentation.shape, dtype = np.uint8)), cmap = 'brg', alpha = 1)        
             plt.xticks([])
@@ -699,23 +818,21 @@ while run:
             divider = make_axes_locatable(ax1)
             cax = divider.append_axes("right", size="5%", pad=0.05)
             cb = plt.colorbar(plot, cax=cax)   
+            cb.ax.tick_params(labelsize=60) 
             cb.remove()
             ax1.invert_yaxis()
             
             try:
                 plt.sca(ax1)
                 plot = plt.pcolormesh(background_image, alpha = 0.2, cmap = 'gray')
-                divider = make_axes_locatable(ax1)
-                cax = divider.append_axes("right", size="5%", pad=0.05)
-                cb = plt.colorbar(plot, cax=cax)   
-                cb.remove()
             except NameError:
                 pass 
             
-            fig.suptitle("Class: " + str(ind2), size = fontsize)
+            plt.tight_layout()
             plt.savefig("Class " + str(ind2) + " With Covariance Table.png")
             plt.close(fig)
             gc.collect()
+        
         
             fig, (ax1) = plt.subplots(1, 1, figsize=(size, size), dpi=300)
     
@@ -738,10 +855,15 @@ while run:
                 cb.remove()
             except NameError:
                 pass 
-            fig.suptitle("Class: " + str(ind2), size = fontsize)
+            plt.tight_layout()
             plt.savefig("Class " + str(ind2) + " Without Covariance Table.png") 
             plt.close(fig)
             gc.collect()
+            
+            
+            
+            
+            
     
     print("Number of unique classes: " + str(len(list(np.unique(segmentation)))))
     print(" ")
@@ -761,5 +883,41 @@ while run:
         raise SystemExit 
 
 
+"""
+full_data = np.load("Filtered Array.npy")
+#training_data = np.load("Initial Training Data.npy")
+covariances = np.load("Final_Model_Covariances.npy")
+means = np.load("Final_Model_Means.npy")
+weights = np.load("Final_Model_Weights.npy")
+precisions_cholesky = np.linalg.cholesky(np.linalg.inv(covariances))
+segmentation = np.load("Segmentation.npy")
+uncertainty = np.load("Log_Probabilities.npy")
 
+DPGMM.covariances_ = covariances 
+DPGMM.means_ = means 
+DPGMM.weights_ = weights 
+DPGMM.precisions_cholesky_ = precisions_cholesky 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 
