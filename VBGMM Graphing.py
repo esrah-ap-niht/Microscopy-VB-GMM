0e8a2b8afd0dc1cf02492d45bf64dd3874d5db26
esrah-ap-niht <86078445+esrah-ap-niht@users.noreply.github.com>
2022-09-27 09:50:40 -0400

    Update VBGMM Graphing.py
    Changing all gridview text to be white with black outline

--- VBGMM Graphing.py
+++ VBGMM Graphing.py
@@ -589,13 +589,15 @@ def plot_GMM(Means, Covariance, Weights, segmentation, uncertainty, background, 
             y = int( (float( row['EDS Stage Y Position (mm)'].decode('utf-8') ) - start_y) * 1_000.0/ float(row['EDS Y Step Size (um)'].decode('utf-8') ) )
             x_range = int( row['EDS Number of X Cells'].decode('utf-8') )
             y_range = int( row['EDS Number of Y Cells'].decode('utf-8') )
-            cmp = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)) 
+            cmp = 255
             title = str( row['EDS Field'].decode('utf-8') )
             
             cv2.rectangle(blank, (x,y), (x+x_range, y+y_range) , cmp, 10 )
             cv2.putText(blank, title, (x + int(x_range/10), y + int(y_range/2) ), cv2.FONT_HERSHEY_TRIPLEX, 3, cmp, 10)
-        
-        plt.imshow(blank)
+            cv2.putText(blank2, title, (x + int(x_range/10), y + int(y_range/2) ), cv2.FONT_HERSHEY_TRIPLEX, 3, (255,255,255), 16)
+            
+        plt.imshow(blank2, cmap = 'binary', alpha = alpha2)
+        plt.imshow(blank, cmap = 'gray', alpha = alpha)
        
         plt.xticks([])
         plt.yticks([])
